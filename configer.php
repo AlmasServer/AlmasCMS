5102dc9cc77e8e7caf902ab5baa40cd8ca3599e8
Ehsan <Ehsan.hesam13@gmail.com>
2012-12-17 19:07:34 -0800

    start publish
    start publish

---
+++ configer.php
@@ -0,0 +1,27 @@
+<?php
+
+ //------------------db
+  $host='localhost';
+  $port='3306'; //empty if default
+  $user='root'
+  $pass='ehsan';
+  $db  ='almascms';
+  $dbprefix = 'almas_';
+  //------------------db
+  $server='almasvps.com';
+  $captcha = '0';
+  $sitename = 'Almas Server';
+  //
+  $smtpuser = '';
+  $smtppass = '';
+  $smtphost = 'localhost';
+  $smtpport = '25';
+  $mailfrom = 'AlmasCms';
+  //
+  
+    
+
+
+
+
+?>
---
+++ lib/db.php
@@ -0,0 +1,40 @@
+﻿<?php
+
+class almasdb{
+ function almas_connection($host,$user,$pass,$port)
+ {
+  $connection = mysql_connect($host,$user,$pass);
+  if (!$connection)
+   return false; 
+ }
+ 
+ function almas_query($query)
+ {
+  $result_array = array();
+  $sql_query = mysql_query($query);
+  $num_rows  = mysql_num_rows($sql_query);
+  for ($i=0; $i<$num_rows ; $i++){
+    if($row=mysql_fetch_array($sql_query)){
+	 $result_array = $row;
+	 
+	}
+  }
+  return $result_array;  
+ }
+ 
+ function almas_selectdb($db,$connection)
+ {
+  $dbselect = mysql_select_db($db,$connection);
+  if(!$dbselect)
+   return false;
+ }
+ 
+ function almas_myadmin($query)
+ {
+  $myadmin = mysql_query($query);
+  if(!$myadmin)
+   return false;
+ }
+}
+
+?> 
